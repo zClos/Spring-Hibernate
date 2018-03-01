71cb480f43589cf3110c7a5400070c837b9be043
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-01 20:05:47 +0300

    Pr9-4 - Programmatic Transactional - test in main method updated

--- src/main/java/com/pvt/App.java
+++ src/main/java/com/pvt/App.java
@@ -1,7 +1,11 @@
 package com.pvt;
 
 import com.pvt.pojos.Department;
+import com.pvt.pojos.Employee;
+import com.pvt.pojos.EmployeeDetail;
 import com.pvt.service.DepartmentService;
+import com.pvt.service.EmployeeDetailService;
+import com.pvt.service.EmployeeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,6 +17,14 @@ public class App {
         Department department = (Department) applicationContext.getBean("department");
         DepartmentService departmentService = (DepartmentService) applicationContext.getBean("departmentService");
         departmentService.saveDepartment(department);
+
+        EmployeeDetailService employeeDetailService = (EmployeeDetailService) applicationContext.getBean("employeeDetailService");
+        EmployeeDetail employeeDetail = (EmployeeDetail) applicationContext.getBean("employeeDetail1");
+        employeeDetailService.deleteEmployeeDetail(employeeDetail);
+
+        EmployeeService employeeService = (EmployeeService) applicationContext.getBean("employeeService");
+        Employee employee = (Employee) applicationContext.getBean("employee1");
+        employeeService.deleteEmployee(employee);
 
     }
 }
