a9bbb6a7cd7c8f5cfb2b911f49f8dcc3eb5a1da6
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-02 01:24:31 +0300

    Pr9-7_-_Spring-Hibernate-JPA-Spring_Data_JPA - update test in main method

--- src/main/java/com/pvt/App.java
+++ src/main/java/com/pvt/App.java
@@ -16,28 +16,25 @@ public class App {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring_Hibernate.xml");
-
-        DepartmentService departmentService = (DepartmentService) applicationContext.getBean("departmentService");
+        DepartmentService departmentService = applicationContext.getBean(DepartmentService.class);
         Department department = (Department) applicationContext.getBean("department");
         departmentService.saveDepartment(department);
         Department getDepartment = departmentService.getDepartment("someDep");
-        System.out.println("\n Getting Department");
-        System.out.println(getDepartment);
+        System.out.println("Getting Department");
+        System.out.println(getDepartment + "\n");
 
-        EmployeeService employeeService = (EmployeeService) applicationContext.getBean("employeeService");
-        Employee employee = employeeService.getEmployee(1);
-        System.out.println("\n Getting Employee");
-        System.out.println(employee);
+        EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
+        Employee employee = employeeService.getEmployeeByFullName("Vasia", "Pupkin").get(0);
+        System.out.println("Getting Employee");
+        System.out.println(employee + "\n");
         employeeService.deleteEmployee(employee);
 
         Set<Meeting> meetings = employee.getMeetings();
-        System.out.println(meetings);
+        System.out.println(meetings + "\n");
 
-        MeetingService meetingService = (MeetingService) applicationContext.getBean("meetingService");
+        MeetingService meetingService = applicationContext.getBean(MeetingService.class);
         meetings.forEach(meeting -> meetingService.deleteMeeting(meeting));
 
         departmentService.deleteDepartment(department);
-
-
     }
 }
