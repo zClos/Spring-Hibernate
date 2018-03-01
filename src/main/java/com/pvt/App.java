b1fbc7940e0e742248cdbd7c66c0a3ac0b51f3a9
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-02 00:01:48 +0300

    Pr9-6 - Spring-Hibernate-JPA - Testing in main method

--- src/main/java/com/pvt/App.java
+++ src/main/java/com/pvt/App.java
@@ -2,29 +2,42 @@ package com.pvt;
 
 import com.pvt.pojos.Department;
 import com.pvt.pojos.Employee;
-import com.pvt.pojos.EmployeeDetail;
+import com.pvt.pojos.Meeting;
 import com.pvt.service.DepartmentService;
-import com.pvt.service.EmployeeDetailService;
 import com.pvt.service.EmployeeService;
+import com.pvt.service.MeetingService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Set;
 
 public class App {
 
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring_Hibernate.xml");
-        Department department = (Department) applicationContext.getBean("department");
-        DepartmentService departmentService = (DepartmentService) applicationContext.getBean("departmentService");
-        departmentService.saveDepartment(department);
 
-        EmployeeDetailService employeeDetailService = (EmployeeDetailService) applicationContext.getBean("employeeDetailService");
-        EmployeeDetail employeeDetail = (EmployeeDetail) applicationContext.getBean("employeeDetail1");
-        employeeDetailService.deleteEmployeeDetail(employeeDetail);
+        DepartmentService departmentService = (DepartmentService) applicationContext.getBean("departmentService");
+        Department department = (Department) applicationContext.getBean("department");
+        departmentService.saveDepartment(department);
+        Department getDepartment = departmentService.getDepartment("someDep");
+        System.out.println("\n Getting Department");
+        System.out.println(getDepartment);
 
         EmployeeService employeeService = (EmployeeService) applicationContext.getBean("employeeService");
-        Employee employee = (Employee) applicationContext.getBean("employee1");
-        employeeService.deleteEmployee(null);
+        Employee employee = employeeService.getEmployee(1);
+        System.out.println("\n Getting Employee");
+        System.out.println(employee);
+        employeeService.deleteEmployee(employee);
+
+        Set<Meeting> meetings = employee.getMeetings();
+        System.out.println(meetings);
+
+        MeetingService meetingService = (MeetingService) applicationContext.getBean("meetingService");
+        meetings.forEach(meeting -> meetingService.deleteMeeting(meeting));
+
+        departmentService.deleteDepartment(department);
+
 
     }
 }
